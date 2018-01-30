ef27c2d3c6a078fa27280559dbf8d780bce1b9f7
MinJaeSim <mj9757@gmail.com>
2018-01-30 16:04:50 +0900

    fragment view add

--- app/src/main/java/ac/ajou/simminje/ateducom/MainActivity.java
+++ app/src/main/java/ac/ajou/simminje/ateducom/MainActivity.java
@@ -1,13 +1,21 @@
 package ac.ajou.simminje.ateducom;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentManager fragmentManager;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.fragment_container, new MainFragment(), "MAIN_FRAGMENT").commit();
     }
 }
---
+++ app/src/main/java/ac/ajou/simminje/ateducom/MainFragment.java
@@ -0,0 +1,29 @@
+package ac.ajou.simminje.ateducom;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MainFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button button = view.findViewById(R.id.hello_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Hello World", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+}
